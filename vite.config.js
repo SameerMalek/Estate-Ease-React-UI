1c7c9f16089baab9f009603da7c1fa67af084496
Mohammad Sameer Malek <113421677+SameerMalek@users.noreply.github.com>
2024-05-28 16:27:42 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,11 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  root: 'src', // Use src as the root directory
   plugins: [react()],
-  base: './',
   build: {
-    outDir: 'dist', 
+    outDir: '../dist', // Output to a top-level dist directory
+    emptyOutDir: true, // Empty the output directory before building
   },
+  publicDir: '../public', // Ensure the public directory is served
 });
